06b3cceb09391dab9b577700caa4b6250c26739d
limasclayton <limasclayton@gmail.com>
2020-05-12 16:40:59 -0300

    Grafics and analysis

--- source/EDA.py
+++ source/EDA.py
@@ -4,11 +4,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
+from sklearn.preprocessing import LabelEncoder
 # caminhos
 dataset_path = "input/census_income_dataset.csv"
 
 # lendo
 df = pd.read_csv(dataset_path)
+df.drop(['capital_loss', 'capital_gain'], inplace=True, axis=1)
+
+# Preprocessing
+encoder = LabelEncoder()
+df['income_level_label'] = encoder.fit_transform(df.income_level)
 
 # initial EDA
 # info(), head(), tail() e describe() from data
@@ -24,6 +30,42 @@ for c in df.columns:
     print('\nColumn:',c)
     print(df[c].value_counts())
     print(df[c].value_counts().shape)
-    print(df.groupby([c,'income_level'])['income_level'].count())
+    print(df.groupby([c])['income_level_label'].sum())
 
+# All possible pairplots
+#sns.pairplot(df, hue='income_level_label')
+#plt.show()
+
+# sex
+#sns.catplot(x='sex', y='income_level_label',kind='bar', data=df)
+
+# race
+#sns.catplot(x='race', y='income_level_label',kind='bar', data=df)
+#sns.catplot(x='race', y='income_level_label', hue='sex', kind='bar', data=df)
+
+# relationship
+#sns.catplot(x='relationship', y='income_level_label',kind='bar', data=df)
+#sns.catplot(x='relationship', y='income_level_label', hue='race', kind='bar', data=df)
+
+# occupation
+#sns.catplot(x='occupation', y='income_level_label',kind='bar', data=df)
+
+# education
+#sns.catplot(x='education', y='income_level_label',kind='bar', data=df)
+
+# workclass
+#sns.catplot(x='workclass', y='income_level_label',kind='bar', data=df)
+
+# age
+#sns.catplot(x='income_level_label', y='age', kind='box', data=df)
+
+# Correlation between hours_per_week and income_level
+#sns.catplot(x='income_level', y='hours_per_week', kind='violin',data=df)
+
+plt.show()
+
+# Conclusions
 # feature capital gain and captain loss apper not to make sense on the model due to their number of zeros.
+# do something with country, but excluding for now
+# Looks like good features race, sex, relationship, occupation(maybe), education, workclass, age
+# Becareful with unbalanced target class
