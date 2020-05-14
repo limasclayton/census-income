1e0a884ca42b5508e733eacbbc9005d53cd33342
limasclayton <limasclayton@gmail.com>
2020-05-14 14:29:27 -0300

    Added feature hours_per_week. Removed SVM.

--- source/model.py
+++ source/model.py
@@ -29,7 +29,7 @@ df = pd.read_csv(dataset_path)
 # Do something with the countries
 
 # FEATURE SELECTION
-features = ['race', 'sex', 'relationship', 'occupation', 'education', 'workclass', 'age']
+features = ['race', 'sex', 'relationship', 'occupation', 'education', 'workclass', 'age', 'hours_per_week']
 X = df[features]
 y = df.income_level
 
@@ -42,15 +42,16 @@ X_train_cat, X_test_cat, y_train_cat, y_test_cat = train_test_split(X, y, test_s
 # MODEL
 
 # Catboost
-#from_file = CatBoostClassifier()
-#from_file.load_model('cat')
 cat_features = ['race', 'sex', 'relationship', 'occupation', 'education', 'workclass']
 
 cat = CatBoostClassifier(cat_features=cat_features, random_seed=RANDOM_STATE)
+#cat.load_model('cat')
+
 cat.fit(X_train_cat, y_train_cat)
 print('CatBoost train score: {:.3f}'.format(cat.score(X_train_cat, y_train_cat)))
 print('CatBoost test score: {:.3f}'.format(cat.score(X_test_cat, y_test_cat)))
-cat.save_model('cat',pool=X_train_cat)
+print(classification_report(y_test_cat, cat.predict(X_test_cat)))
+#cat.save_model('cat',pool=X_train_cat)
 
 # HistGradientBoostingClassifier
 param_distributions_hgb = {
@@ -71,29 +72,3 @@ print(classification_report(y_test, hgb_CV.best_estimator_.predict(X_test)))
 #plt.show()
 #plot_confusion_matrix(hgb_CV.best_estimator_, X_test, y_test)
 #plt.show()
-
-# Support Vector Machine
-svm_pipe = Pipeline([
-    ('scaler', StandardScaler()),
-    ('clf', SVC())
-    ])
-
-param_distributions_svm = [{
-    #'clf__kernel' : ['linear', 'poly', 'rbf', 'sigmoid'],
-    'clf__C' : np.logspace(-3, 1, 10),
-    'clf__gamma' : ['scale', 'auto'],
-    #'clf__verbose' : [True]
-    #'clf__degree' : [2, 3, 4, 5],
-    #'clf__tol' : np.logspace(-4,-2,10)
-    }]
-    
-svm_CV = RandomizedSearchCV(svm_pipe, param_distributions=param_distributions_svm, cv=10, n_jobs=-1, verbose=1, random_state=RANDOM_STATE)
-svm_CV.fit(X_train, y_train)
-print('-' * 100)
-print('SVM train score: {:.3f}'.format(svm_CV.score(X_train, y_train)))
-print('SVM test score: {:.3f}'.format(svm_CV.score(X_test, y_test)))
-print('SVM best params: {0}'.format(svm_CV.best_params_))
-print('SVM supports: {0}'.format(svm_CV.best_estimator_.named_steps['clf'].support_))
-print('SVM support vector: {0}'.format(svm_CV.best_estimator_.named_steps['clf'].support_vectors_))
-print('SVM number of support vector: {0}'.format(svm_CV.best_estimator_.named_steps['clf'].n_support_))
-print(classification_report(y_test, svm_CV.best_estimator_.predict(X_test)))
